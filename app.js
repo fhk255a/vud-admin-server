ed0637fb2ac810498cc6e8b47840cf34608966d2
Joker <hongkai.fan@orderplus.com>
2020-03-19 21:59:20 +0800

    完善

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const Koa = require('koa');
 const app = new Koa();
 const Router = require('./init/router');
 const BodyParser = require('koa-bodyparser');
+const cors = require('koa-cors');
 const tryCatch = require('./lib/tryCatch');
 // 初始化所有路由
 // 注册异常捕获
 app.use(tryCatch);
+// 支持跨域
+app.use(cors());
 Router.init(app);
+
+
 // 解析post的data
 app.use(BodyParser());
 app.listen(3000);
--- app/api/h5/index.js
+++ app/api/h5/index.js
@@ -3,8 +3,10 @@ const Success = require('../../../lib/success');
 const MyError = require('../../../lib/error');
 const query = require('../../../lib/query');
 const router = new Router();
-router.get('/list', async (ctx,next)=>{
-  let content = 'SELECT * FROM `page` LIMIT 0, 1000';
+router.get('/h5/list', async (ctx,next)=>{
+  const params = ctx.query;
+  let content = `SELECT * FROM 'page' WHERE 1 LIMIT ${(params.current-1)*params.size},${params.size};`
+  // let content = 'SELECT * FROM `page` LIMIT 0, 1000';
   await query(content).then(res=>{
     ctx.body = new Success(res);
   })

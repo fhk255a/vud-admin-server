d4c9d711f8081fedff20365c62212f1c4865b74f
Joker <215537936@qq.com>
2020-03-21 01:25:55 +0800

    完善订单详情接口 待加强

--- app/api/order/index.js
+++ app/api/order/index.js
@@ -3,8 +3,9 @@ const Success = require('../../../lib/success');
 const MyError = require('../../../lib/error');
 const query = require('../../../lib/query');
 const TABLE_NAME = 'order';
-const {WHERE_TOTAL,SEARCH_PAGE,SEARCH,INSERT} = require('../../../lib/sql');
+const {WHERE_TOTAL,SEARCH_PAGE,SEARCH,INSERT,UPDATE} = require('../../../lib/sql');
 const router = new Router();
+// 获取订单列表
 router.get('/order/list',async (ctx,next)=>{
   const SQL = SEARCH_PAGE(TABLE_NAME,ctx.query);
   const TOTAL = WHERE_TOTAL(TABLE_NAME,ctx.query);
@@ -19,6 +20,39 @@ router.get('/order/list',async (ctx,next)=>{
     body.data = res;
   });
   ctx.body = new Success(body);
+})
+// 取消订单
+router.post('/order/cancel',async (ctx,next)=>{
+  if(!ctx.request.body.id){
+    ctx.body = new MyError('订单ID不能为空',400,1000);
+    return;
+  }
+  let key ={
+    cancelDate:new Date().getTime(),
+    isCancel:true,
+    cancelCode:'运营后台取消',
+    cancelUser:'Joker',
+    orderStatus:'CANCEL'
+  }
+  const SQL = UPDATE(TABLE_NAME,key,{id:ctx.request.body.id});
+  await query(SQL).then(res=>{
+    ctx.body = new Success();
+  }).catch(err=>{
+    ctx.body = err;
+  });
+})
+// 查询订单详情
+router.get('/order/details',async (ctx,next)=>{
+  if(!ctx.query.id){
+    ctx.body = new MyError('订单ID不能为空',400,1000);
+    return;
+  }
+  const SQL = SEARCH(TABLE_NAME,ctx.query);
+  await query(SQL).then(res=>{
+    ctx.body = new Success(res[0]);
+  }).catch(err=>{
+    ctx.body = err;
+  });
 })
 // 添加
   // const SQL = INSERT('order',ctx.request.body.data[0],ctx.request.body.data);
